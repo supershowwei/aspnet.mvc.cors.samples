a6ce551460ac32f372bae980e68cdacd47bde322
supershowwei <supershowwei@gmail.com>
2016-05-21 22:25:48 +0800

    complete asp.net mvc cors sample code

---
+++ AspNetMvcCORSSamples/AspNetMvcCORSSamples/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AspNetMvcCORSSamples.Filters;
+
+namespace AspNetMvcCORSSamples.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowCORS]
+        public ActionResult Hello(string name)
+        {
+            return Json(new { Greeting = $"Hi, {name}" });
+        }
+    }
+}
---
+++ AspNetMvcCORSSamples/AspNetMvcCORSSamples/Filters/AllowCORSAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvcCORSSamples.Filters
+{
+    public class AllowCORSAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type");
+            filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Methods", "POST");
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
